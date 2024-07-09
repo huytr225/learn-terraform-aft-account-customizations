0c552d76e00350d528256b1a64ce21d3ea3f6f55
HuyTD <huytr225@gmail.com>
2024-07-09 10:22:24 +0700

    update

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,18 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  bucket     = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_ownership_controls" "sandbox_ownership_controls" {
+  bucket = aws_s3_bucket.sandbox_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "sandbox_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.sandbox_ownership_controls]
+  bucket = aws_s3_bucket.sandbox_bucket.id
   acl    = "private"
 }
